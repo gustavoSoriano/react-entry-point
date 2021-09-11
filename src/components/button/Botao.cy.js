ad0884d998ac0f15d7bb5ed14ce04d45320385bd
Gustavo Soriano <sorianox2010@hotmail.com>
2021-09-11 05:27:52 -0300

    cypress TDD

---
+++ src/components/button/Botao.cy.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { mount } from "@cypress/react";
+import { Button } from "./index";
+
+describe("Button", () => {
+    it("renders the Button", () => {
+        let count = 0;
+
+        mount(
+            <Button
+                title={"Botão"}
+                onClick={() => {
+                    count += 1;
+                }}
+            />
+        );
+
+        cy.contains("Botão").dblclick();
+
+        cy.contains("Botão")
+            .should(($el) => {
+                expect($el).to.be.visible;
+            })
+            .then(() => {
+                expect(count).equal(2);
+            });
+    });
+});
